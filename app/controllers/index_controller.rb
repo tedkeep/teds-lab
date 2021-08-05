4949e11bb183c0fed76fbfe9f0f74f09850c753f
Ted <47033086+tedkeep@users.noreply.github.com>
2021-08-05 22:00:17 +0100

    Add blog post link

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,3 +1,7 @@
 class IndexController < ApplicationController
-  def index; end
+  BLOG = Blog.new(root: Rails.root.join('app', 'blog_posts'), cache: Rails.env.production?)
+
+  def index
+    @posts = BLOG.posts.first(3)
+  end
 end
