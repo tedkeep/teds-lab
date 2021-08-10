0c07427e5d8a865f0fba39ba765f300704f80ad1
Ted <47033086+tedkeep@users.noreply.github.com>
2021-08-10 18:09:05 +0100

    Add wiggle animation

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': {
+            transform: 'rotate(-3deg)'
+          },
+          '50%': {
+            transform: 'rotate(3deg)'
+          },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 5s ease-in-out infinite',
+      }
+    },
   },
   variants: {
     extend: {},
